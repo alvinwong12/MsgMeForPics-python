2d27c9800ca400a7016305c986d32947bad3c9ca
alvinwong12 <alvinwong312@gmail.com>
2018-02-06 22:42:40 -0500

    Added emoji in response

--- utils/response.py
+++ utils/response.py
@@ -1,7 +1,10 @@
 from werkzeug import *
 
 def generateTwilioResponse(sms, media=None):
-	body = "Here is a picture of %s" %sms['body']
+	body = "Here is a picture of %s %s" %(sms['body'], emoji("happy"))
+	if not media:
+		body = "I found nothing %s" %emoji("cry")
+		
 	if sms['param'].lower() == "sms" and media:
 		body += ". Media URL: %s" %str(media)
 		media = None
@@ -24,3 +27,13 @@ def generateMediaURL(media, size="medium"):
 	}
 	mediaURL =  "https://farm%s.staticflickr.com/%s/%s_%s_%s.jpg" %(str(media['farm']), str(media['server']), str(media['id']), str(media['secret']), SIZES[size])
 	return mediaURL;
+
+def emoji(mood=None):
+	EMOJI = {
+		'cry': '1F622',
+		'happy': '1F604',
+		'no-emo': '1F636'
+	}
+	# Default return no expression face
+	emoji_str = "\U000%s" %EMOJI.get(mood.lower(), '1F636')
+	return emoji_str.decode('unicode-escape')
