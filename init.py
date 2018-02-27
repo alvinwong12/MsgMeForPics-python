f6991b0586799983fc1363cea2d52fd53e144100
alvinwong12 <alvinwong312@gmail.com>
2018-02-26 22:42:58 -0500

    reconfig for celery

--- init.py
+++ init.py
@@ -2,13 +2,14 @@ from flask import Flask, request
 from celery import Celery
 from utils.config import Config
 from utils.model import DynamoDB
+import os
 parser = Config().getParser()
 parser.read('config/server.ini')
 
 app = Flask(__name__)
 
-app.config['CELERY_BROKER_URL'] = parser.get('celery', 'CELERY_BROKER_URL')
-app.config['CELERY_RESULT_BACKEND'] = parser.get('celery', 'CELERY_RESULT_BACKEND')
+app.config['CELERY_BROKER_URL'] = parser.get('celery', 'CELERY_BROKER_URL') if os.environ['PYTHON_ENV'] == "development" else os.environ['REDIS_URL']
+app.config['CELERY_RESULT_BACKEND'] = parser.get('celery', 'CELERY_RESULT_BACKEND') if os.environ['PYTHON_ENV'] == "development" else os.environ['REDIS_URL']
 app.config['CELERY_TASK_SERIALIZER'] = "json"
 
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
